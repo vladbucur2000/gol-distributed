f253be59f01257970f8575b0a664344df52de3c2
Edward <nechitoaiageorgeedward@gmail.com>
2020-12-01 20:01:52 +0200

    Stage2 - 1st Step Done

--- gol/controller.go
+++ gol/controller.go
@@ -1,13 +1,14 @@
 package gol
 
 import (
-	"net"
-	"fmt"
-	"strings"
 	"bufio"
+	"fmt"
+	"net"
 	"strconv"
-)
+	"strings"
 
+	"uk.ac.bris.cs/gameoflife/util"
+)
 
 type distributorChannels struct {
 	events      chan<- Event
@@ -19,17 +20,102 @@ type distributorChannels struct {
 	outputWorld chan<- [][]byte
 }
 
-func read(conn *net.Conn) {
+func read(c distributorChannels, conn *net.Conn) {
 	reader := bufio.NewReader(*conn)
-	msg, _ := reader.ReadString('\n')
-	fmt.Println(msg)
-}
+	for {
+		msg, _ := reader.ReadString('\n')
+		//CELLFLIPPED RECEIVED
+		if msg[0] == 'c' && msg[1] == 'f' {
+			i := 2
+			x := 0
+			y := 0
+			turn := 0
+			for i < len(msg) && msg[i] != ' ' {
+				y = y*10 + (int(msg[i]) - '0')
+				i++
+			}
+			i++
+			for i < len(msg) && msg[i] != ' ' {
+				x = x*10 + (int(msg[i]) - '0')
+				i++
+			}
+			i++
+			for i < len(msg) && msg[i] != '\n' {
+				turn = turn*10 + (int(msg[i]) - '0')
+				i++
+			}
+			c.events <- CellFlipped{
+				turn,
+				util.Cell{
+					X: x,
+					Y: y,
+				},
+			}
+			//	break
+			//TURN COMPLETE
+		} else if msg[0] == 't' && msg[1] == 'c' {
+			i := 2
+			turn := 0
+			for i < len(msg) && msg[i] != '\n' {
+				turn = turn*10 + (int(msg[i]) - '0')
+				i++
+			}
+			c.events <- TurnComplete{
+				turn,
+			}
 
+			//FINAL TURN COMPLETE
+		} else if msg[0] == 'f' && msg[1] == 't' && msg[2] == 'c' {
+
+			//AICI TREBUIE MODIFICAT PENTRU "BUG"
+			i := 3
+			turn := 0
+			var alive []util.Cell
+			for i < len(msg) && msg[i] != ' ' {
+				turn = turn*10 + (int(msg[i]) - '0')
+				i++
+			}
+
+			i += 3
+			for i < len(msg) && msg[i] != '\n' {
+				x := 0
+				y := 0
+				j := i
+
+				for j < len(msg) && msg[j] != ';' {
+					x = x*10 + (int(msg[j]) - '0')
+					j++
+				}
+
+				j += 3
+
+				for j < len(msg) && msg[j] != ' ' {
+					y = y*10 + (int(msg[j]) - '0')
+					j++
+				}
+
+				alive = append(alive, util.Cell{
+					X: x,
+					Y: y,
+				})
+				j += 3
+				i = j
+			}
+
+			c.events <- FinalTurnComplete{
+				turn,
+				alive,
+			}
+			return
+
+		}
+	}
+
+}
 
 func numberToString(nr int) string {
 	return strconv.Itoa(nr)
 }
-
 
 func convertToString(world [][]byte, p Params) string {
 	var data []string
@@ -38,8 +124,6 @@ func convertToString(world [][]byte, p Params) string {
 	ws := numberToString(p.ImageWidth)
 	turn := numberToString(p.Turns)
 	thread := numberToString(p.Threads)
-
-
 
 	data = append(data, hs)
 	data = append(data, "\n")
@@ -50,19 +134,18 @@ func convertToString(world [][]byte, p Params) string {
 	data = append(data, thread)
 	data = append(data, "\n")
 
-
 	for i := 0; i < p.ImageHeight; i++ {
 		for j := 0; j < p.ImageWidth; j++ {
-			
+
 			if world[i][j] == 255 {
 				data = append(data, "1")
 			}
 			if world[i][j] == 0 {
 				data = append(data, "0")
 			}
-			
+
 		}
-		data = append (data, "\n")
+		data = append(data, "\n")
 	}
 
 	data = append(data, "gata!!\t")
@@ -73,22 +156,19 @@ func convertToString(world [][]byte, p Params) string {
 // distributor divides the work between workers and interacts with other goroutines.
 func controller(p Params, c distributorChannels) {
 
-  	conn, _ := net.Dial("tcp", "127.0.0.1:8080")
-	  
-//	for {
-    //	fmt.Println("Enter text: ")
-    //citesc lumea
+	conn, _ := net.Dial("tcp", "127.0.0.1:8080")
+
+	//citesc lumea
 	world := make([][]byte, p.ImageHeight)
 	for i := range world {
 		world[i] = make([]byte, p.ImageWidth)
-	  }
+	}
 
 	c.ioCommand <- ioInput
 
-
 	c.ioFilename <- strings.Join([]string{strconv.Itoa(p.ImageWidth), strconv.Itoa(p.ImageHeight)}, "x")
 	// TODO: For all initially alive cells send a CellFlipped Event.
-// SA EXTRAG INPUTUL IN MATRICE SI SA L TRANSFORM IN STRING CA SA L TRIMIT LA ENGINE
+	// SA EXTRAG INPUTUL IN MATRICE SI SA L TRANSFORM IN STRING CA SA L TRIMIT LA ENGINE
 	for i := 0; i < p.ImageHeight; i++ {
 		for j := 0; j < p.ImageWidth; j++ {
 			val := <-c.ioInput
@@ -99,28 +179,14 @@ func controller(p Params, c distributorChannels) {
 	//text := util.MatricesToString(world, nil, p.ImageWidth, p.ImageHeight)
 	text := convertToString(world, p)
 
-	//fmt.Println(text)
 	fmt.Fprintf(conn, text)
-	for {
-		read(&conn)
-	}
-
-	//}
-
-
-//	turn := 0
-
-	// TODO: Execute all turns of the Game of Life.
-	// TODO: Send correct Events when required, e.g. CellFlipped, TurnComplete and FinalTurnComplete.
-	//		 See event.go for a list of all events.
-
+	//for {
+	read(c, &conn)
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
-//	c.events <- StateChange{turn, Quitting}
+	//	c.events <- StateChange{turn, Quitting}
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
 }
-
-
--- server/engine.go
+++ server/engine.go
@@ -1,44 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
-	"bufio"
-	"strings"
 	"strconv"
+	"strings"
 )
 
+func mod(x, m int) int {
+	return (x + m) % m
+}
+func calculateNeighbours(ImageHeight, ImageWidth, x, y int, world [][]byte) int {
+	neighbours := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if i != 0 || j != 0 {
+				if world[mod(y+i, ImageHeight)][mod(x+j, ImageWidth)] != 0 {
+					neighbours++
+				}
+			}
+		}
+	}
+	return neighbours
+}
 
-func stringToMatrix (msg string, conn *net.Conn) {
+func stringToMatrix(msg string, conn *net.Conn) {
 	i := 0
 	height := 0
 	width := 0
 	turn := 0
 	thread := 0
 
-	for (i < len(msg) && msg[i] != '\n') {
-		height = height * 10 + (int(msg[i]) - '0')
+	for i < len(msg) && msg[i] != '\n' {
+		height = height*10 + (int(msg[i]) - '0')
 		i++
 	}
 	i++
-	for (i < len(msg) && msg[i] != '\n') {
-		width = width * 10 + (int(msg[i]) - '0')
+	for i < len(msg) && msg[i] != '\n' {
+		width = width*10 + (int(msg[i]) - '0')
 		i++
 	}
 	i++
-	for (i < len(msg) && msg[i] != '\n') {
-		turn = turn * 10 + (int(msg[i]) - '0')
+	for i < len(msg) && msg[i] != '\n' {
+		turn = turn*10 + (int(msg[i]) - '0')
 		i++
 	}
 	i++
-	for (i < len(msg) && msg[i] != '\n') {
-		thread = thread * 10 + (int(msg[i]) - '0')
+	for i < len(msg) && msg[i] != '\n' {
+		thread = thread*10 + (int(msg[i]) - '0')
 		i++
 	}
-	fmt.Println("height:", height)
-	fmt.Println("width:", width)
-	fmt.Println("turn: ", turn)
-	fmt.Println("thread: ", thread)
 
 	i++
 	nr := i
@@ -46,80 +58,231 @@ func stringToMatrix (msg string, conn *net.Conn) {
 	world := make([][]byte, height)
 	for i := range world {
 		world[i] = make([]byte, width)
-	  }
-
-	for i := 0; i < 16; i++ {
-	  for j := 0; j < 16; j++ {
-		  world[i][j] = msg[nr] - '0'
-		  nr++
-		  fmt.Print(world[i][j])
-		  fmt.Print (" ")
-		  
-	  }
-	  fmt.Println()
-	  nr++
 	}
 
-	makeTurn(height, width, turn, thread, world, conn)
-//	fmt.Println("gata cumetre")
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			world[i][j] = msg[nr] - '0'
+			nr++
 
+		}
+		nr++
+	}
+
+	playTheGame(height, width, turn, thread, world, conn)
 
 }
 
-
 func createCellFlipped(i int, j int, turn int) string {
 	var data []string
+	data = append(data, "cf")
 	data = append(data, strconv.Itoa(i))
 	data = append(data, " ")
 	data = append(data, strconv.Itoa(j))
 	data = append(data, " ")
 	data = append(data, strconv.Itoa(turn))
+	data = append(data, "\n")
 
 	return strings.Join(data, "")
 
 }
 
-func makeTurn (height int, width int, turn int, thread int, world [][]byte, conn *net.Conn) {
-	currentTurn := 0
-	str := ""
-	for i := 0; i < height; i++ {
+func createFinalTurnComplete(turn int, world [][]byte, heigth int, width int) string {
+	var data []string
+	data = append(data, "ftc")
+	data = append(data, strconv.Itoa(turn))
+	data = append(data, " ")
+	for i := 0; i < heigth; i++ {
 		for j := 0; j < width; j++ {
-			if (world[i][j] == 1) {
-			//	SEND EVENT TO CONTROLLER
-				str = createCellFlipped(i, j, currentTurn)
-				fmt.Println("salut", str)
-				fmt.Fprintln(*conn, str)
+			if world[i][j] != 0 {
+				data = append(data, "x:")
+				data = append(data, strconv.Itoa(j))
+				data = append(data, ";")
+				data = append(data, "y:")
+				data = append(data, strconv.Itoa(i))
+				data = append(data, " ")
 			}
+		}
+	}
+	data = append(data, "\n")
+	return strings.Join(data, "")
+}
+
+func createTurnComplete(turn int) string {
+	var data []string
+	data = append(data, "tc")
+	data = append(data, strconv.Itoa(turn))
+	data = append(data, "\n")
+	return strings.Join(data, "")
+}
+
+func worker(originalWorld [][]byte, turn int, ImageHeight, ImageWidth int, start int, end int, height int, outChannel chan byte, inputChannel chan byte, conn *net.Conn) {
+	world := make([][]byte, ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, ImageWidth)
+	}
+	for i := start; i < end; i++ {
+		for j := 0; j < ImageWidth; j++ {
+			world[i][j] = <-inputChannel
+		}
+	}
+
+	newWorld := make([][]byte, ImageHeight)
+	for i := range newWorld {
+		newWorld[i] = make([]byte, ImageWidth)
+	}
+
+	for i := start; i < end; i++ {
+		for j := 0; j < ImageWidth; j++ {
+			neighbours := calculateNeighbours(ImageHeight, ImageWidth, j, i, originalWorld)
+			if world[i][j] != 0 {
+				if neighbours == 2 || neighbours == 3 {
+					newWorld[i][j] = 1
+				} else {
+					newWorld[i][j] = 0
+					msg := createCellFlipped(i, j, turn)
+					fmt.Fprintln(*conn, msg)
+				}
+			} else {
+				if neighbours == 3 {
+					newWorld[i][j] = 1
+					msg := createCellFlipped(i, j, turn)
+
+					fmt.Fprintln(*conn, msg)
+				} else {
+					newWorld[i][j] = 0
+				}
+			}
+		}
+
+	}
+
+	for i := start; i < end; i++ {
+		for j := 0; j < ImageWidth; j++ {
+			outChannel <- newWorld[i][j]
 		}
 	}
 
 }
 
+func startWorker(workerHeight int, turn int, ImageHeight, ImageWidth int, world [][]byte, start int, end int, thread int, outChannel chan byte, inputChannel chan byte, conn *net.Conn) {
+
+	go worker(world, turn, ImageHeight, ImageWidth, start, end, workerHeight, outChannel, inputChannel, conn)
+
+	for i := start; i < end; i++ {
+		for j := 0; j < ImageWidth; j++ {
+
+			inputChannel <- world[i][j]
+		}
+
+	}
+}
+func myVisualiseMatrix(world [][]byte, ImageWidth, ImageHeight int) {
+	for i := 0; i < ImageHeight; i++ {
+		for j := 0; j < ImageWidth; j++ {
+			fmt.Print(world[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func playTheGame(ImageHeight int, ImageWidth int, Turns int, Threads int, world [][]byte, conn *net.Conn) {
+
+	workingWorld := make([][]byte, ImageHeight)
+	for i := range world {
+		workingWorld[i] = make([]byte, ImageWidth)
+	}
+
+	for i := 0; i < ImageHeight; i++ {
+		for j := 0; j < ImageWidth; j++ {
+			val := world[i][j]
+			workingWorld[i][j] = val
+			if val != 0 {
+				msg := createCellFlipped(i, j, 0)
+				fmt.Fprintln(*conn, msg)
+			}
+		}
+	}
+
+	turn := 0
+	for ; turn < Turns; turn++ {
+
+		// select {
+		// //AICI INTRA KEY URILE
+		// default:
+		// }
+
+		workerHeight := ImageHeight / Threads
+		outChannel := make([]chan byte, Threads)
+
+		newWorld := make([][]byte, ImageHeight)
+		for i := range workingWorld {
+			newWorld[i] = make([]byte, ImageWidth)
+		}
+		for thread := 0; thread < Threads-1; thread++ {
+			inputChannel := make(chan byte)
+			outChannel[thread] = make(chan byte)
+			start := workerHeight * thread
+			end := start + workerHeight
+			startWorker(workerHeight, turn, ImageHeight, ImageWidth, workingWorld, start, end, thread, outChannel[thread], inputChannel, conn)
+			for i := start; i < end; i++ {
+				for j := 0; j < ImageWidth; j++ {
+					newWorld[i][j] = <-outChannel[thread]
+				}
+			}
+		}
+
+		inputChannel := make(chan byte)
+		outChannel[Threads-1] = make(chan byte)
+		start := workerHeight * (Threads - 1)
+		end := ImageHeight
+
+		startWorker(workerHeight, turn, ImageHeight, ImageWidth, workingWorld, start, end, Threads-1, outChannel[Threads-1], inputChannel, conn)
+		for i := start; i < end; i++ {
+			for j := 0; j < ImageWidth; j++ {
+				newWorld[i][j] = <-outChannel[Threads-1]
+			}
+		}
+
+		for i := 0; i < ImageHeight; i++ {
+			for j := 0; j < ImageWidth; j++ {
+				workingWorld[i][j] = newWorld[i][j]
+			}
+		}
+		turnCompleteString := createTurnComplete(turn)
+
+		fmt.Fprintln(*conn, turnCompleteString)
+		//myVisualiseMatrix(workingWorld, ImageWidth, ImageHeight)
+
+	}
+
+	finalTurnCompleteString := createFinalTurnComplete(turn, workingWorld, ImageHeight, ImageWidth)
+	fmt.Fprintln(*conn, finalTurnCompleteString)
+
+}
 
 func handleConnection(conn *net.Conn) {
 	reader := bufio.NewReader(*conn)
 
-	//for {
-		msg, _ := reader.ReadString('\t')
-	//	fmt.Printf(msg)
-		stringToMatrix(msg, conn) 
-	//	fmt.Printf("\n\n\n")
-		fmt.Fprintln(*conn, "am primit cumetre")
-//	}
-}
+	msg, _ := reader.ReadString('\t')
 
+	stringToMatrix(msg, conn)
+
+	fmt.Fprintln(*conn, "am primit cumetre")
+
+}
 
 func main() {
 	ln, err := net.Listen("tcp", ":8080")
 
-	if (err != nil) {
+	if err != nil {
 		fmt.Println("eroare cumetre")
 	}
 
 	for {
-		conn, er := ln.Accept();
+		conn, er := ln.Accept()
 
-		if (er != nil) {
+		if er != nil {
 			fmt.Println("eroare cumetre2")
 		}
 
